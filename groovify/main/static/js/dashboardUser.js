15f8d26be11e5c5e1672d56fd8a1ff14bc11ec8f
EzequielDiaz00 <arashixz00@gmail.com>
2024-11-17 02:11:08 -0600

    Parcial_III

--- groovify/main/static/js/dashboardUser.js
+++ groovify/main/static/js/dashboardUser.js
@@ -6,57 +6,57 @@ const songInfo = document.getElementById('song_info');
 
 // Actualiza la información de la canción
 function updateSongInfo(index) {
-  const songName = playlist[index].split('/').pop();
-  songInfo.innerHTML = `<p>${decodeURIComponent(songName.replace('.mp3', ''))}</p>`;
+    const songName = playlist[index].split('/').pop();
+    songInfo.innerHTML = `<p>${decodeURIComponent(songName.replace('.mp3', ''))}</p>`;
 }
 
 // Reproduce la canción actual
 function playSong() {
-  audio.play();
-  updateSongInfo(currentIndex);
+    audio.play();
+    updateSongInfo(currentIndex);
 }
 
 // Cambia la canción al hacer clic en una de la lista
 document.querySelectorAll('.song-item').forEach((item, index) => {
-  item.addEventListener('click', () => {
-    currentIndex = index; // Actualiza el índice
-    audio.src = playlist[currentIndex];
-    playSong();
-  });
+    item.addEventListener('click', () => {
+        currentIndex = index; // Actualiza el índice
+        audio.src = playlist[currentIndex];
+        playSong();
+    });
 });
 
 // Pausa la canción
 function pauseSong() {
-  audio.pause();
+    audio.pause();
 }
 
 // Detiene la canción
 function stopSong() {
-  audio.pause();
-  audio.currentTime = 0;
+    audio.pause();
+    audio.currentTime = 0;
 }
 
 // Avanza a la siguiente canción
 function skipSong() {
-  currentIndex = (currentIndex + 1) % playlist.length;
-  audio.src = playlist[currentIndex];
-  playSong();
+    currentIndex = (currentIndex + 1) % playlist.length;
+    audio.src = playlist[currentIndex];
+    playSong();
 }
 
 // Retrocede a la canción anterior
 function backSong() {
-  currentIndex = (currentIndex - 1 + playlist.length) % playlist.length;
-  audio.src = playlist[currentIndex];
-  playSong();
+    currentIndex = (currentIndex - 1 + playlist.length) % playlist.length;
+    audio.src = playlist[currentIndex];
+    playSong();
 }
 
 // Actualiza la barra de progreso
 audio.ontimeupdate = () => {
-  progressBar.value = (audio.currentTime / audio.duration) * 100;
+    progressBar.value = (audio.currentTime / audio.duration) * 100;
 };
 
 progressBar.addEventListener('input', (e) => {
-  audio.currentTime = (e.target.value / 100) * audio.duration;
+    audio.currentTime = (e.target.value / 100) * audio.duration;
 });
 
 // Controles
