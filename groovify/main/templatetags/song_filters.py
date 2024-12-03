6ff8c15c816d3086c843b0ddd5143b7d05b2302b
EzequielDiaz00 <arashixz00@gmail.com>
2024-12-02 19:40:52 -0600

    Parcial_III

--- groovify/main/templatetags/song_filters.py
+++ groovify/main/templatetags/song_filters.py
@@ -10,10 +10,29 @@ def extract_song_name(url):
     """
     if not url:
         return "Canción desconocida"
+    
     # Quitar los parámetros de la URL (después del "?")
     clean_url = url.split('?')[0]
-    # Extraer el nombre del archivo
+    
+    # Extraer el nombre del archivo (después del último "/")
     file_name = clean_url.split('/')[-1]
+    
     # Decodificar caracteres especiales y quitar la extensión
     song_name = urllib.parse.unquote(file_name).replace('.mp3', '').replace('_', ' ')
+    
     return song_name
+
+
+@register.filter
+def sort_by_name(songs):
+    """
+    Ordena las canciones por nombre de canción (después del guion).
+    """
+    return sorted(songs, key=lambda x: x.split(' - ')[1] if ' - ' in x else x)
+
+@register.filter
+def sort_by_artist(songs):
+    """
+    Ordena las canciones por nombre de artista (antes del guion).
+    """
+    return sorted(songs, key=lambda x: x.split(' - ')[0] if ' - ' in x else x)
